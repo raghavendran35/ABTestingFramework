d2a8ef4852ff1e836f9b376cb7f99ac868f75a42
Sachin <sraghavendran@berkeley.edu>
2018-06-13 10:24:27 -0700

    all

---
+++ hilbert/hilbert/src/main/java/hello/User.java
@@ -0,0 +1,64 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Integer id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String company;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company= company;
+	}
+
+
+}
