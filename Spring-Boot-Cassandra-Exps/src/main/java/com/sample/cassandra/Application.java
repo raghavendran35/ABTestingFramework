51bfb9b954c6d1183e621e62837e877d3e663b80
raghavendran35 <34849276+raghavendran35@users.noreply.github.com>
2018-06-25 10:16:07 -0700

    Add files via upload

---
+++ Spring-Boot-Cassandra-Exps/src/main/java/com/sample/cassandra/Application.java
@@ -0,0 +1,28 @@
+package com.sample.cassandra;
+
+import com.datastax.driver.core.utils.UUIDs;
+import com.sample.cassandra.repo.ExpRepo;
+import com.sample.cassandra.entity.Experiment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    private ExpRepo repository;
+
+//s    @Override
+//    public void run(String... args) throws Exception {
+//        // Example data for first insert. It can be deleted as soon as things go well
+//        this.repository.save(new Experiment(UUIDs.timeBased(), "firstOne", "06.14.2018","06.14.2018", 3));
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
